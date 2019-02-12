ff70f4ef24c2207fc518114dca502cde702bb44b
Stuart Wheater <stuart.wheater@arjuna.com>
2019-02-12 12:35:22 +0000

    Updated opal management functions

--- R/getOpals.R
+++ R/getOpals.R
@@ -6,13 +6,13 @@
 #' This way no matter what the user calls his opal login object it will be captured. 
 #' @keywords internal
 #' @return a list of opal object obtained after login into the servers
-#' @author Gaye,A.
-#' 
+#' @author Amadou Gaye, Paul Burton (updated 15/10/18). THIS IS VERSION TO USE 8/2/19.
 getOpals <- function(){
+  
   # get the names of all the objects in the current work environment
   objs <- ls(name=.GlobalEnv)
   
-  # check which of the objects is a list (the opal objects are kept in a list)
+  # check which of the object is a list (the opal objects are kept in a list)
   if(length(objs) > 0){
     opalist <- vector('list')
     cnt <- 0
@@ -24,10 +24,12 @@ getOpals <- function(){
         list2check <- eval(parse(text=objs[i]))
         if(length(list2check) > 0){
           cl2 <- class(list2check[[1]])
-          if(cl2 == 'opal'){
-            cnt <- cnt + 1
-            opalist[[cnt]] <- objs[i]
-            flag <- 1
+          for(s in 1:length(cl2)){
+            if(cl2[s] == 'opal'){
+              cnt <- cnt + 1
+              opalist[[cnt]] <- objs[i]
+              flag <- 1
+            }
           }
         }
       }
@@ -35,17 +37,18 @@ getOpals <- function(){
     if(flag == 1){ 
       if(length(opalist) > 1){
          flag <- 2
-         return(list("flag"=flag, "opals"=unlist(opalist)))
+         return(list("flag"=flag, "opals"=unlist(opalist), "opals.list"=unlist(opalist)))
       }else{
         pp <- opalist[[1]] 
         opals <- eval(parse(text=pp))
-        return(list("flag"=flag, "opals"=opals))
+        return(list("flag"=flag, "opals"=opals, "opals.list"=unlist(opalist)))
       }
     }else{
-      return(list("flag"=flag, "opals"=NULL))
+      return(list("flag"=flag, "opals"=NULL, "opals.list"=NULL))
     }
   }else{
-    return(list("flag"=flag, "opals"=NULL))
+    return(list("flag"=flag, "opals"=NULL, "opals.list"=NULL))
   }
   
 }
+#getOpals
